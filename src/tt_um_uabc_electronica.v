f1b94fc76ac5b27a9e80e6ceefbaaafeb82f7f0d
Rogelio <149198850+rogeermv@users.noreply.github.com>
2023-10-29 20:07:08 -0700

    Update tt_um_uabc_electronica.v

--- src/tt_um_uabc_electronica.v
+++ src/tt_um_uabc_electronica.v
@@ -1,33 +1,15 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 29.10.2023 18:40:57
-// Design Name: 
-// Module Name: tt_um_uabc_electronica
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module tt_um_uabc_electronica(
-    input clk,
-    output reg PULSO,
-    input ACTIVAR,
-    
-    output reg [6:0] seg,
-    output reg [3:0] an
-    );
+module tt_um_uabc_electronica #( parameter MAX_COUNT = 24'd10_000_000 ) (
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
     
     reg reloj;
     reg [25:0] cont = 0;
